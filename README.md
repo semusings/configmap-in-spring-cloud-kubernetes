00801d3dfb56181d793313160efd7ac6ef7d36cf
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-06-21 08:54:11 +0545

    fix: db connection

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Configmap in Spring Cloud Kubernetes
 
 Blog: <https://bhuwanupadhyay.github.io/blog/configmap-in-spring-cloud-kubernetes>
+
+
+## References
+- https://docs.spring.io/spring-data/r2dbc/docs/current/reference/html/#reference
--- src/main/kotlin/io/github/bhuwanupadhyay/example/SpringBoot.kt
+++ src/main/kotlin/io/github/bhuwanupadhyay/example/SpringBoot.kt
@@ -2,6 +2,7 @@ package io.github.bhuwanupadhyay.example
 
 import io.r2dbc.spi.ConnectionFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -14,7 +15,7 @@ import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 class SpringBoot
 
 fun main(args: Array<String>) {
-	runApplication<SpringBoot>(*args)
+    runApplication<SpringBoot>(*args)
 }
 
 @Suppress("SpringJavaInjectionPointsAutowiringInspection")
@@ -22,11 +23,15 @@ fun main(args: Array<String>) {
 @EnableR2dbcRepositories
 class R2dbcConfiguration {
 
-	@Bean
-	fun initializer(connectionFactory: ConnectionFactory): ConnectionFactoryInitializer {
-		val initializer = ConnectionFactoryInitializer()
-		initializer.setConnectionFactory(connectionFactory)
-		initializer.setDatabasePopulator(ResourceDatabasePopulator(ClassPathResource("db-schema.sql")))
-		return initializer
-	}
+    @Bean
+    fun initializer(connectionFactory: ConnectionFactory, r2dbc: R2dbcProperties): ConnectionFactoryInitializer {
+        val initializer = ConnectionFactoryInitializer()
+        initializer.setConnectionFactory(connectionFactory)
+        if (r2dbc.url.startsWith("r2dbc:h2")) {
+            initializer.setDatabasePopulator(ResourceDatabasePopulator(ClassPathResource("schema-h2.sql")))
+        } else if (r2dbc.url.startsWith("r2dbc:postgresql")) {
+            initializer.setDatabasePopulator(ResourceDatabasePopulator(ClassPathResource("schema-postgresql.sql")))
+        }
+        return initializer
+    }
 }
